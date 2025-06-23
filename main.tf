b255858c731c36878783a50851bbb95dcbdeadea
Naren <lnaren532@gmail.com>
2025-06-23 18:34:00 +0530

    terraform_aws_sg

---
+++ main.tf
@@ -0,0 +1,14 @@
+#roboshop-dev-frontend
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+        default = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "sg_name" {
+    type = string
+}
+
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+
+variable "sg_tags" {
+    type = map(string)
+    default = {}
+  
+}
+
+variable "description" {
+    type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
